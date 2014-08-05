53e5d10cebc69c963ba4d708df9e7fc85b8b175a
miguelaferreira <miguelferreira@me.com>
2014-08-05 08:52:57 +0200

    Patched version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jgoulah@gmail.com'
 license          'BSD'
 description      'Installs/Configures x11vnc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.0.3001'
 
 supports         'centos'
